c3c3e626568b5f344f26695efc5d26613c17f5f5
error4071 <sergey_tsoy87@mail.ru>
2023-08-14 21:09:16 +0300

    linter correction1

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,12 +1,14 @@
+
 package hexlet.code;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DifferTest {
     private static String file1;
@@ -14,17 +16,17 @@ public class DifferTest {
     private static String file1yml;
     private static String file2yml;
     private static String correctResult;
-    private static String PlainCorrectResult;
-    private static String JsonCorrectResult;
-    private static String YmlCorrectResult;
+    private static String plainCorrectResult;
+    private static String jsonCorrectResult;
+    private static String ymlCorrectResult;
     private static String defaultFormat;
 
     @BeforeAll
     public static void beforeAll() throws Exception {
         correctResult = Files.readString(Paths.get("./src/test/resources/correctResult"));
-        PlainCorrectResult = Files.readString(Paths.get("./src/test/resources/PlainCorrectResult"));
-        JsonCorrectResult = Files.readString(Paths.get("./src/test/resources/JsonCorrectResult"));
-        YmlCorrectResult = Files.readString(Paths.get("./src/test/resources/YmlCorrectResult"));
+        plainCorrectResult = Files.readString(Paths.get("./src/test/resources/plainCorrectResult"));
+        jsonCorrectResult = Files.readString(Paths.get("./src/test/resources/jsonCorrectResult"));
+        ymlCorrectResult = Files.readString(Paths.get("./src/test/resources/ymlCorrectResult"));
         defaultFormat = "stylish";
     }
     @Test
@@ -46,41 +48,41 @@ public class DifferTest {
         file1 = "./src/test/resources/json/file1.json";
         file2 = "./src/test/resources/json/file2.json";
         String result = Differ.generate(file1, file2, "plain");
-        assertThat(result).isEqualTo(PlainCorrectResult);
+        assertThat(result).isEqualTo(plainCorrectResult);
     }
     @Test
     public void jsonTestJson() throws Exception {
         file1 = "./src/test/resources/json/file1.json";
         file2 = "./src/test/resources/json/file2.json";
         String result = Differ.generate(file1, file2, "json");
-        assertThat(result).isEqualTo(JsonCorrectResult);
+        assertThat(result).isEqualTo(jsonCorrectResult);
     }
     @Test
     public void ymlTestDefault() throws IOException {
         file1yml = "./src/test/resources/yml/file1.yml";
         file2yml = "./src/test/resources/yml/file2.yml";
         String result = Differ.generate(file1yml, file2yml, defaultFormat);
-        assertThat(result).isEqualTo(YmlCorrectResult);
+        assertThat(result).isEqualTo(ymlCorrectResult);
     }
     @Test
     public void ymlTestStylish() throws Exception {
         file1yml = "./src/test/resources/yml/file1.yml";
         file2yml = "./src/test/resources/yml/file2.yml";
         String result = Differ.generate(file1yml, file2yml, "stylish");
-        assertThat(result).isEqualTo(YmlCorrectResult);
+        assertThat(result).isEqualTo(ymlCorrectResult);
     }
     @Test
     public void ymlTestPlain() throws Exception {
         file1yml = "./src/test/resources/yml/file1.yml";
         file2yml = "./src/test/resources/yml/file2.yml";
         String result = Differ.generate(file1yml, file2yml, "plain");
-        assertThat(result).isEqualTo(PlainCorrectResult);
+        assertThat(result).isEqualTo(plainCorrectResult);
     }
     @Test
     public void ymlTestJson() throws Exception {
         file1yml = "./src/test/resources/yml/file1.yml";
         file2yml = "./src/test/resources/yml/file2.yml";
         String result = Differ.generate(file1yml, file2yml, "json");
-        assertThat(result).isEqualTo(JsonCorrectResult);
+        assertThat(result).isEqualTo(jsonCorrectResult);
     }
 }
