75afc427c484e6dfb231cff4e8e5c5e628316d0a
error4071 <sergey_tsoy87@mail.ru>
2023-08-18 20:55:07 +0300

    Parser edit

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -10,23 +10,10 @@ import java.util.Map;
 
 public class Parser {
 
-        public static Map<String, Object> parser(String content, String formatData) throws Exception {
-            return switch (formatData) {
-                case "yml", "yaml" -> parseYaml(content);
-                case "json" -> parseJson(content);
-                default -> throw new Exception("Unknown format: '" + formatData + "'");
-            };
-        }
+        public static Map<String, Object> parser(String content, String format) throws JsonProcessingException {
 
-        public static Map<String, Object> parseJson(String json) throws JsonProcessingException {
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(json, new TypeReference<>() {
-            });
-        }
-
-        public static Map<String, Object> parseYaml(String yaml) throws JsonProcessingException {
             ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-            return mapper.readValue(yaml, new TypeReference<>() {
+            return mapper.readValue(content, new TypeReference<>() {
             });
         }
     }
