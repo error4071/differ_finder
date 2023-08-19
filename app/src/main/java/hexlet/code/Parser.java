a5cb6bf65d60b8dcd5839583a2c3e16366dadc33
error4071 <sergey_tsoy87@mail.ru>
2023-08-19 16:38:33 +0300

    Parser edit5

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parser(String content, format) throws IOException {
+    public static Map<String, Object> parser(String content, String format) throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
         return objectMapper.readValue(content, new TypeReference<>() {
