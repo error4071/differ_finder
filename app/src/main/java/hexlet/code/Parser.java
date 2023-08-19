315d321d65cbd90db45d0854cfa948fd826e140e
error4071 <sergey_tsoy87@mail.ru>
2023-08-19 16:33:10 +0300

    Parser edit4

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,19 +1,18 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-	public static Map<String, Object> parser(String content, String format) throws IOException {
+    public static Map<String, Object> parser(String content, format) throws IOException {
 
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        return mapper.readValue(content, new TypeReference<>() {});
-	}
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        return objectMapper.readValue(content, new TypeReference<>() {
+        });
+    }
 }
 
 
