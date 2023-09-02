acbb0032554a11badec5980a35c586edfcbe08a9
error4071 <sergey_tsoy87@mail.ru>
2023-09-03 01:04:08 +0500

    restore code2

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,18 +2,31 @@ package hexlet.code;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parser(String content, String format) throws IOException {
+    public static Map<String, Object> parser(String fileContent, String fileFormat) throws IOException {
+        return switch (fileFormat) {
+            case "json" -> getDataJson(fileContent);
+            case "yml", "yaml" -> getDataYaml(fileContent);
+            default -> throw new IOException("Unknown file extension! -> " + fileFormat);
+        };
+    }
 
-        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        return objectMapper.readValue(content, new TypeReference<>() {
-        });
+    private static Map<String, Object> getDataJson(String fileContent) throws IOException {
+        ObjectMapper jm = new JsonMapper();
+
+        return jm.readValue(fileContent, new TypeReference<>() { });
+    }
+
+    private static Map<String, Object> getDataYaml(String fileContent) throws IOException {
+        ObjectMapper ym = new YAMLMapper();
+
+        return ym.readValue(fileContent, new TypeReference<>() { });
     }
 }
-
-
